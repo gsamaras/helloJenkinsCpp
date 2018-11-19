60e1ba72fff54a881d86001ec1a3bf683540fbce
George Samaras <georgios.samaras@airbus.com>
2018-11-19 10:15:15 +0100

    fds

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkinss\n";
 
   cout << "I have successfully built and run\n";
   return 0;
