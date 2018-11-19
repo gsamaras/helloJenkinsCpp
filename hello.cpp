0eeeb0497e2bf2aaad1baae6a81bb9f9927a10b6
gsamaras <georgesamarasdit@gmail.com>
2018-11-19 10:23:18 +0100

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "sas\n";
+  cout << "Thsi shouldc \n";
 
   cout << "I have successfully built and run\n";
   return 0;
