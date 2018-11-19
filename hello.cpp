63e60d36c66a57088c41e3158fa708ec5c158423
gsamaras <georgesamarasdit@gmail.com>
2018-11-19 10:20:36 +0100

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "This is Jenkins\n";
+  cout << "I am Jenkins\n";
 
   cout << "I have successfully built and run\n";
   return 0;
