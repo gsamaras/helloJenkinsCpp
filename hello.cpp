522decf0b2a2b9ab80d4a111c6f7d0cc56dfc1ab
George Samaras <georgios.samaras@airbus.com>
2018-11-19 10:22:06 +0100

    foo

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "I am Jenkins\n";
+  cout << "sas\n";
 
   cout << "I have successfully built and run\n";
   return 0;
